14426492bd4b07fa41dc2cbdd5012a03acde8868
gillchristian <gillchristiang@gmail.com>
2020-05-28 00:22:53 +0200

    feat(slides): Longer version

--- Me.js
+++ Me.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
-import {vsDark as theme} from '@code-surfer/themes'
+import React from "react";
+import styled from "styled-components";
+import { vsDark as theme } from "@code-surfer/themes";
 
-import avatar from './images/avatar.png'
+const Avatar = styled.div`
+  background-color: #e57a5a;
+  width: 40px;
+  height: 40px;
+
+  display: inline;
+  border-radius: 100%;
+`;
 
 const Container = styled.div`
   display: flex;
@@ -22,19 +29,11 @@ const Container = styled.div`
     text-decoration: none;
     cursor: pointer;
   }
-
-  & img {
-    width: 40px;
-    height: 40px;
-
-    display: inline;
-    border-radius: 100%;
-  }
-`
+`;
 
 const Me = () => (
   <Container>
-    <img src={avatar} alt="avatar" />
+    <Avatar />
     <div className="name">
       <a
         href="https://twitter.com/gillchristian"
@@ -43,12 +42,16 @@ const Me = () => (
       >
         Christian Gill
       </a>
-      {' @ '}
-      <a href="https://catawiki.com" target="_blank" rel="noopener noreferrer">
+      {" @ "}
+      <a
+        href="https://catawiki.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Catawiki
       </a>
     </div>
   </Container>
-)
+);
 
-export default Me
+export default Me;
